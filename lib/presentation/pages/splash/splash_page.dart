46ad793c194388a03b204b9500b84290689e040d
tanyapog <megatatb@yandex.ru>
2022-05-12 19:25:48 +0300

    SpashPage - первая страница приложения. С неё авторизованные пользователи попадают на список задач, а неавторизованные - на страницу логина

---
+++ lib/presentation/pages/splash/splash_page.dart
@@ -0,0 +1,24 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_list/application/auth/auth_bloc.dart';
+import 'package:todo_list/presentation/routes/router.gr.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) =>
+        state.map(
+          initial: (_) => null,
+          authenticated: (_) => AutoRouter.of(context)
+            .replace(const TasksOverviewRoute()),
+          unauthenticated: (_) => AutoRouter.of(context)
+            .replace(const SignInRoute()),
+        ),
+      child: const Scaffold(
+        body: Center(child: CircularProgressIndicator(),),
+      ),
+    );
+  }
+}
--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,11 +1,13 @@
 import 'package:auto_route/annotations.dart';
 import 'package:todo_list/presentation/pages/sign_in/sign_in_page.dart';
+import 'package:todo_list/presentation/pages/splash/splash_page.dart';
 import 'package:todo_list/presentation/tasks/tasks_overview_page.dart';
 
 @MaterialAutoRouter(
     replaceInRouteName: 'Page,Route',
     routes: <AutoRoute> [
-      MaterialRoute(page: SignInPage, initial: true),
+      MaterialRoute(page: SplashPage, initial: true),
+      MaterialRoute(page: SignInPage),
       MaterialRoute(page: TasksOverviewPage),
     ]
 )
